9099f21d5fe3e7d78d2826ac532cd6f14744fa08
Kittiphop Pitiwittayakul <kittiphop293@gmail.com>
2022-09-14 11:26:39 +0700

    9/14/2022

---
+++ App Fetch API.js
@@ -0,0 +1,52 @@
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovie = async () => {
+    try {
+      const response = await fetch("https://reactnative.dev/movies.json");
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, [])
+
+  return (
+    <View style={{ flex: 1, padding: 20 }}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item }) => (
+            <Text>
+              {item.title},{item.releaseYear}
+            </Text>
+          )}
+        />
+      )}
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
